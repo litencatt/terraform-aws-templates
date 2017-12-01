8ddb789c9e8aa8d32bea002e2fc7f2ea3462f63d
Nakamura Kosuke <ncl0709@gmail.com>
2017-12-01 22:32:50 +0900

    create public subnet

--- main.tf
+++ main.tf
@@ -14,3 +14,12 @@ resource "aws_vpc" "vpc-1" {
     Name = "vpc-1"
   }
 }
+
+resource "aws_subnet" "vpc-1-public-subnet" {
+  vpc_id            = "${aws_vpc.vpc-1.id}"
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-northeast-1a"
+  tags {
+    Name = "vpc-1-public-subnet"
+  }
+}
