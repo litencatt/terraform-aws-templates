bf3f7ba27f24de009fee02861ee8c21e537eb400
Nakamura Kosuke <ncl0709@gmail.com>
2017-12-02 11:20:50 +0900

    exec '$ terraform fmt'

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,6 @@
 resource "aws_vpc" "vpc-1" {
   cidr_block = "10.0.0.0/16"
+
   tags {
     Name = "vpc-1"
   }
@@ -9,6 +10,7 @@ resource "aws_subnet" "vpc-1-public-subnet" {
   vpc_id            = "${aws_vpc.vpc-1.id}"
   cidr_block        = "10.0.1.0/24"
   availability_zone = "ap-northeast-1a"
+
   tags {
     Name = "vpc-1-public-subnet"
   }
@@ -16,6 +18,7 @@ resource "aws_subnet" "vpc-1-public-subnet" {
 
 resource "aws_internet_gateway" "vpc-1-igw" {
   vpc_id = "${aws_vpc.vpc-1.id}"
+
   tags {
     Name = "vpc-1-igw"
   }
@@ -23,10 +26,12 @@ resource "aws_internet_gateway" "vpc-1-igw" {
 
 resource "aws_route_table" "vpc-1-public-rt" {
   vpc_id = "${aws_vpc.vpc-1.id}"
+
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = "${aws_internet_gateway.vpc-1-igw.id}"
   }
+
   tags {
     Name = "vpc-1-public-rt"
   }
