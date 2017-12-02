d3ba666c90674e5b825278a1b44851f7a8b685c7
Nakamura Kosuke <ncl0709@gmail.com>
2017-12-02 10:01:08 +0900

    add network interface

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,11 @@
 resource "aws_instance" "web-server" {
   ami           = "ami-da9e2cbc"
   instance_type = "t2.micro"
-  associate_public_ip_address = true
+
+  network_interface {
+    network_interface_id = "${aws_network_interface.web-server-ni.id}"
+    device_index = 0
+  }
 
   tags {
     Name = "web-server"
