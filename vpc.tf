f9e8c22dec2ec0a2f34f3d3776622249c347064b
Nakamura Kosuke <ncl0709@gmail.com>
2017-12-02 12:24:15 +0900

    add dns resolver settings

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,7 @@
 resource "aws_vpc" "vpc-1" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
   tags {
     Name = "vpc-1"
